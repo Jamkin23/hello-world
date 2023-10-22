c91809dae008e3169c2d189eebc8f0cac324bfed
James Kinder <james.kinder@pcc.edu>
2023-10-22 10:38:06 -0700

    Added name as author, not sure if I need to, just being safe

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,6 +1,10 @@
 package edu.pcc.cis.helloworld;
 import java.util.Date;
 
+/*
+*   Author: James Kinder
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
